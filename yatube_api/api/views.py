89b6becf234061a6388e2c5e159817fc7ab4db49
Матвей Строков <strokov.matvei@yandex.ru>
2022-06-11 19:57:43 +0300

    second attempt before second review

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, permissions, filters, mixins
 from rest_framework.pagination import LimitOffsetPagination
 
-from posts.models import Post, Group, Follow, User
+from posts.models import Post, Group, Follow
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (
     CommentSerializer, FollowSerializer, GroupSerializer, PostSerializer
